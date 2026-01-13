25b21a751e9c9ce44bd420ac867685e073474e42
einarformica-code <einar.formica@iesnervion.es>
2026-01-13 14:06:18 +0100

    Cambios Einar

--- PruebaConflicto/src/Ejercicio/Prueba.java
+++ PruebaConflicto/src/Ejercicio/Prueba.java
@@ -4,13 +4,14 @@ public class Prueba {
 
 	public static void main(String[] args) {
 
-		int p = 1;
+		int p = 1 * 2;
 		
-		int h = 8;
+		int h = 8 + p;
 		
-		final double K = 7.8;
+		final double K = 7.8; 
 		
 		System.out.println("Hola mi nombre es Gonzalo Prada");
+		System.out.println("Hola me llamo Einar");
 		
 		System.out.println(p+h-K);
 		
